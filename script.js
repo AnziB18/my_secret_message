ba7a031b5052aeaa6b36bb8186ccea197e9cb701
Bernd Anzenberger <anzenbernd@gmail.com>
2022-02-02 19:43:38 +0100

    simple code for messages and code with object and functions

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-let randomMessage=Math.floor(Math.random()*5+1);
-let msg='';
 
+let msg=[];
+let message='';
+const motivationMessage = {
+        _firstWord:['It ', 'He ', 'She ', 'I ', 'You ', 'They '],
+        _verb: ['sing ', 'dance ', 'cry ', 'fly ', 'drive ', 'go '],
+        _bindword:['with ', 'with ', 'with ', 'with ', 'with ', 'with '],
+        _subject: ['my mother', 'your father', 'her parents', 'his sister', 'her brother', 'our uncle'],
+        _end:['.', '?', '!','.', '?', '!' ]
+}
+for (let item in motivationMessage){
+let randomMessage = Math.floor(Math.random()*5); // move into for - scope, because random is only once and not in every loop if it is in global scope
+switch(randomMessage){ 
+    case 0:
+        msg += motivationMessage[item][0];
+        break;
+    case 1:
+        //if (motivationMessage[item][0] === 'She '|| motivationMessage[item][0] === 'It ' || motivationMessage[item][0] === 'He '){
+        msg += motivationMessage[item][1];
+        //} else {
+        //msg += motivationMessage[item][1]; 
+        //}
+        break;
+    case 2:
+        msg += motivationMessage[item][2];
+        break;
+    case 3:
+        msg += motivationMessage[item][3];
+        break;
+    case 4:
+        msg += motivationMessage[item][4];
+        break;
+    case 5:
+        msg += motivationMessage[item][5];
+        break;
+    default:
+        msg += 'You got something wrong'
+        break;
+}; 
+}
+console.log(msg);
+
+/*
 switch(randomMessage){
     
     case 1:
@@ -76,5 +116,20 @@ switch(randomMessage){
     default:
         msg2 += 'You got something wrong'
         break;
+};*/
+//console.log(msg + msg1 + msg2);
+//let newMsg=[];
+//newMsg.push(msg[0]);
+//newMsg.join('')
+//console.log(newMsg)
+//console.log(newMsg);
+/*
+if (msg[0] === 'She ' || msg[0] === 'It ' || msg[0] === 'He ')
+{
+    msg[1].push('s');
+    
+} else {
+    msg[1];
 };
-console.log(msg + msg1 + msg2);
+
+console.log(msg)*/
